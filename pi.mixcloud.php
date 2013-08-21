3b769466a3c8cffd2f56059827dea8c04c812f3f
Jeremy Sexton <jeremy@jeremysexton.net>
2013-08-21 19:42:47 -0400

    Initial commit

---
+++ pi.mixcloud.php
@@ -0,0 +1,26 @@
+<?php
+class Plugin_mixcloud extends Plugin {
+
+    var $meta = array(
+        'name'       => 'Mixcloud',
+        'version'    => '1.0',
+        'author'     => 'Jeremy Sexton',
+        'author_url' => 'http://jeremysexton.net'
+    );
+
+    public function mix()
+    {
+      $width = $this->fetchParam('width', null);
+      $height = $this->fetchParam('height', null);
+      $url = $this->fetchParam('url', null);
+    
+      if ($url) {
+	      $output = '
+	      <iframe width="'.$width.'" height="'.$height.'" src="//www.mixcloud.com/widget/iframe/?feed='.$url.'&embed_type=widget_standard" frameborder="0"></iframe>
+	      ';
+	      return $output;
+      }
+      
+    }
+
+}
